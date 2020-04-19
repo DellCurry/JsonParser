a70ff4fdb77f4854da1e1fe1ba9ba55ec6c6c0b3
zhaoyonghe <909672941@qq.com>
2020-04-19 13:08:13 -0400

    add json node

--- JSONNode.h
+++ JSONNode.h
@@ -1,1 +1,41 @@
 #pragma once
+#include<string>
+#include<unordered_map>
+#include<vector>
+
+typedef std::unordered_map<std::string, JSONNode> NodeMap;
+typedef std::vector<JSONNode> NodeVec;
+
+class JSONNode
+{
+public:
+	JSONNode();
+	~JSONNode();
+
+private:
+    enum {
+        NUMBER,
+        STRING,
+        ARRAY,
+        OBJECT,
+        TRUE,
+        FALSE,
+        NUL,
+        UNKNOWN
+    } type;
+    std::string name;
+    union {
+        double num;
+        std::string str;
+        NodeMap nodeMap;
+        NodeVec nodeVec;
+    };
+};
+
+JSONNode::JSONNode()
+{
+}
+
+JSONNode::~JSONNode()
+{
+}
